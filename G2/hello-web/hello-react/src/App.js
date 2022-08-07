3500ca57f88b18c7e683390f7a7af5aded70d124
HelitonCalgaroto <whelyton_calgarotto@hotmail.com>
2022-08-06 23:12:57 -0300

    atualização

---
+++ G2/hello-web/hello-react/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+
+  const title = <div><h1>Filho de sapo</h1></div>;
+  const message = <h1>Sapinho é KAKKAKAAKKAKAKAKAKAKAKK</h1>
+
+  return (
+    <div>
+        {title}
+        {message}
+    </div>
+  );
+}
+
+export default App;
